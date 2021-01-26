1ebf12a5fbef0b6ac338c6e86fba7b6329957d69
SarahAlgwaiz <60787364+SarahAlgwaiz@users.noreply.github.com>
2021-01-26 18:54:17 +0300

    firebase fix

--- Sayyar2021/Assets/FirebaseInit.cs
+++ Sayyar2021/Assets/FirebaseInit.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+using Firebase;
+using Firebase.Analytics;
+public class FirebaseInit : MonoBehaviour
 {
-    private Rigidbody player;
     // Start is called before the first frame update
     void Start()
     {
-        
+FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+    FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+});
     }
 
     // Update is called once per frame
