419d62e2d8a78c224b7e68a0bf244dc39405d527
SarahAlgwaiz <60787364+SarahAlgwaiz@users.noreply.github.com>
2021-02-02 21:59:24 +0300

    join script (yet to be tested)

--- Sayyar2021/Assets/Scripts/JoinRoomScript.cs
+++ Sayyar2021/Assets/Scripts/JoinRoomScript.cs
@@ -4,11 +4,18 @@ using Photon.Pun;
 using Photon.Realtime;
 using System;
 using UnityEngine.SceneManagement;
-
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+using System.Threading.Tasks;
 namespace com.cactusteam.Sayyar{
     public class JoinRoomScript: MonoBehaviourPunCallbacks{
     
-    
+     [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public DatabaseReference reference;
+    private FirebaseUser user;
+
 [SerializeField]
 private GameObject joinRoomView;
 [SerializeField]
@@ -16,22 +23,32 @@ private Button roomCodeConfirmButton;
 [SerializeField]
 private TMPro.TMP_InputField roomNumField;
 
-
+private bool valid ;
 
     public void OnClickRoomCodeConfirmButton(){
         JoinRoom();
     }
-        public override void OnConnectedToMaster(){
-            Debug.Log("connected to master");
-        }
         
         public void JoinRoom(){    
                string roomCode = roomNumField.text;
                if(roomCode.Length!=5){
                    Debug.Log("Room Code length should be 5");
                }
-          
-            PhotonNetwork.JoinRoom(roomCode);
+            //    PhotonNetwork.JoinRoom(roomCode);
+                   FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //If they are avalible Initialize Firebase
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+
             
         }
         public override void OnDisconnected(DisconnectCause cause){
@@ -42,11 +59,36 @@ private TMPro.TMP_InputField roomNumField;
         }
         public override void OnJoinRoomFailed(short returnCode, string message){
             Debug.Log("join room failed" + message);
+            valid = false;
         }
         public override void OnJoinedRoom(){
             Debug.Log("Success! joined room");
             SceneManager.LoadSceneAsync("SolarSystemGame");
         }
 
+  async void InitializeFirebase(){
+        reference = FirebaseDatabase.GetInstance("https://sayyar-2021-default-rtdb.firebaseio.com/").RootReference;
+        valid = await isRoomCodeValid();
+        if(valid){
+            SceneManager.LoadSceneAsync("game"); //has joined the room and will now go to the waiting room
+        }
+
+            }
+
+        async Task<bool> isRoomCodeValid(){
+            Query doesRoomCodeExist = reference.Root.Child("WaitingRooms").OrderByChild("RoomCode").OrderByValue().EqualTo(roomNumField);
+            DataSnapshot result = Task.Run(() => doesRoomCodeExist.GetValueAsync()).Result;
+            if(result.Exists){
+                PhotonNetwork.JoinRoom(roomNumField.text);
+                if(!PhotonNetwork.InRoom){
+                    return false;
+                }
+                DatabaseReference parent = doesRoomCodeExist.Reference.Parent.Child("KindergartnersID").Child("Kindergartner" + PhotonNetwork.CurrentRoom.PlayerCount);
+                await Task.Run(() => parent.SetValueAsync(user.UserId));
+                return true;
+            }
+            return false;
+
+        }
     }
 }
