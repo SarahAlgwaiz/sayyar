df8ae0aec5c4f3d544ca6d4e0542ed54ce57ca81
SarahAlgwaiz <60787364+SarahAlgwaiz@users.noreply.github.com>
2021-02-02 20:01:08 +0300

    writing WRoom and VPRoom data to Firebase

--- Sayyar2021/Assets/Scripts/CreateRoomScript.cs
+++ Sayyar2021/Assets/Scripts/CreateRoomScript.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
-using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
-using System;
 using UnityEngine.SceneManagement;
-
+using Firebase;
+using Firebase.Database;
+using Firebase.Auth;
+using System.Threading.Tasks;
 namespace com.cactusteam.Sayyar{
     public class CreateRoomScript: MonoBehaviourPunCallbacks{
-        [Tooltip("Maximum number of players in each room. When a room is full, new players can't join in. Therefore a new room will be created for the new player.")]
+
+
+         [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public DatabaseReference reference;
+    private FirebaseUser user;
+        
+    [Tooltip("Maximum number of players in each room. When a room is full, new players can't join in. Therefore a new room will be created for the new player.")]
     [SerializeField]
     private byte maxPlayersPerRoom = 4;
     
@@ -27,6 +35,20 @@ private int roomNumber;
         }
         public override void OnCreatedRoom(){
             roomCodeCreateField.text = "Room Number: "+roomNumber;
+                FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                //If they are avalible Initialize Firebase
+                InitializeFirebase();
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+
         }
          private void Start() {
             createRoom();
@@ -48,5 +70,37 @@ private int roomNumber;
            Debug.Log("failed to create room");
            createRoom();
         }
+    
+    async void InitializeFirebase(){
+        reference = FirebaseDatabase.GetInstance("https://sayyar-2021-default-rtdb.firebaseio.com/").RootReference;
+        string key = await writeVirtualPlayroomData();
+        await writeWaitingRoomData(key);
+            }
+        public async Task<string> writeVirtualPlayroomData(){
+        reference = reference.Child("VirtualPlayrooms").Push();
+        var key = reference.Key;
+        reference = reference.Root;
+        await Task.Run(() =>reference.Child("VirtualPlayrooms").Child(key).Child("NumOfPlayers").SetValueAsync(maxPlayersPerRoom));
+        await Task.Run(() =>reference.Child("VirtualPlayrooms").Child(key).Child("VirtualPlayroomID").SetValueAsync(key));
+
+       //the following code should have a valid value (not null) otherwise the subsequent async calls will fail
+
+        //await Task.Run(() =>reference.Child("VirtualPlayrooms").Child(key).Child("Game").SetValueAsync(key));
+        //await Task.Run(() => reference.Child("VirtualPlayrooms").Child(key1).Child("HostID").SetValueAsync(user.UserId));
+        return key;
+    }
+    public async Task writeWaitingRoomData(string VPkey){
+         reference = reference.Child("WaitingRooms").Push();
+        var key = reference.Key;
+        reference = reference.Root;
+       await Task.Run(() =>  reference.Child("WaitingRooms").Child(key).Child("WaitingRoomID").SetValueAsync(key));
+       await Task.Run(() =>  reference.Child("WaitingRooms").Child(key).Child("RoomCode").SetValueAsync(roomNumber));
+       await Task.Run(() =>  reference.Child("VirtualPlayrooms").Child(VPkey).Child("WaitingRoomID").SetValueAsync(key));
+       //the following code should have a valid value (not null) otherwise the subsequent async calls will fail
+
+        //await Task.Run(() =>reference.Child("WaitingRooms").Child(key).Child("HostID").SetValueAsync(user.UserId));
+        //await Task.Run(() =reference.Child("WaitingRooms").Child("1").Child("KindergartnerIDs").setValueAsync("something")); 
+        //await Task.Run(() =reference.Child("WaitingRooms").Child(""+roomNumber).Child("VideoID").SetValueAsync(videoID));    
+    }
     }
 }
