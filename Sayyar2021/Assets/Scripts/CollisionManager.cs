9244d450ce42c2a3eec9e8adb3f216a88b66356c
SarahAlgwaiz <60787364+SarahAlgwaiz@users.noreply.github.com>
2021-03-21 19:14:07 +0300

    sync v6 - RPC

--- Sayyar2021/Assets/Scripts/CollisionManager.cs
+++ Sayyar2021/Assets/Scripts/CollisionManager.cs
@@ -3,21 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class CollisionManager : MonoBehaviour, IPunObservable
+public class CollisionManager : MonoBehaviour
 {
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo messageInfo){
-       if(stream.IsWriting){
-    stream.SendNext(PlanetsOnPlane.isPlanetInserted);
-    }
-    else
-    {
-      bool[] planetsArr =  (bool[]) stream.ReceiveNext();
-      for(int i =0; i<planetsArr.Length; i++){
-        if(planetsArr[i])
-        PlanetsOnPlane.isPlanetInserted[i] = planetsArr[i];
-      }
-  }
-    }
      private void OnTriggerEnter(Collider other) {
        Debug.Log("collided outside");
        Debug.Log("other " + other.gameObject.name);
@@ -25,52 +12,77 @@ public class CollisionManager : MonoBehaviour, IPunObservable
         if((other.gameObject.name + "(Clone)") == name){
          other.GetComponent<Renderer>().material = GetComponent<Renderer>().material;
             Debug.Log("collided inside");
-            this.gameObject.SetActive(false);
             Debug.Log("after destroy");
-            other.GetComponent<PhotonView>().RequestOwnership();
+           this.gameObject.SetActive(false);
             switch(name){
                  case "Mercury(Clone)":
-                 PlanetsOnPlane.isPlanetInserted[0] = true;
-                 this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{0});
                  Debug.Log("planet 0 done");
                  break;
                  case "Venus(Clone)":
-                 PlanetsOnPlane.isPlanetInserted[1] = true;
-                 this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{1});
                  Debug.Log("planet 1 done");
                  break;
                  case "Earth(Clone)":
-                 PlanetsOnPlane.isPlanetInserted[2] = true;
-                 this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{2});
                 Debug.Log("planet 2 done");
                  break;
                   case "Mars(Clone)":
-                 PlanetsOnPlane.isPlanetInserted[3] = true;
-                 this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{3});
                  Debug.Log("planet 3 done");
                   break;
                   case "Jupiter(Clone)":
-                  PlanetsOnPlane.isPlanetInserted[4] = true;
-                  this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{4});
                   Debug.Log("planet 4 done");
                   break;
                    case "Saturn(Clone)":
-                 PlanetsOnPlane.isPlanetInserted[5] = true;
-                 this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{5});
                  Debug.Log("planet 5 done");
                    break;
                    case "Uranus(Clone)":
-                  PlanetsOnPlane.isPlanetInserted[6] = true;
-                  this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{6});
                   Debug.Log("planet 6 done");
                    break;
                     case "Neptune(Clone)":
-                  PlanetsOnPlane.isPlanetInserted[7] = true;
-                  this.gameObject.SetActive(false);
+                 this.gameObject.GetComponent<PhotonView>().RPC("updatePlanetInsertion",RpcTarget.All,new object[]{7});
                    Debug.Log("planet 7 done");
                     break;
                     default: break;
             }
     }
+        }
+
+
+        [PunRPC]
+        public void updatePlanetInsertion(int planetNumber){
+          Debug.Log("inside RPC");
+        switch(planetNumber){
+          case 0:
+          PlanetsOnPlane.isPlanetInserted[0] = true;
+          break;
+          case 1:
+          PlanetsOnPlane.isPlanetInserted[1] = true;
+          break;
+          case 2:
+          PlanetsOnPlane.isPlanetInserted[2] = true;
+          break;
+          case 3:
+          PlanetsOnPlane.isPlanetInserted[3] = true;
+          break;
+          case 4:
+          PlanetsOnPlane.isPlanetInserted[4] = true;
+          break;
+          case 5:
+          PlanetsOnPlane.isPlanetInserted[5] = true;
+          break;
+          case 6:
+          PlanetsOnPlane.isPlanetInserted[6] = true;
+          break;
+          case 7:
+          PlanetsOnPlane.isPlanetInserted[7] = true;
+          break;
+          default:
+          break;
+        }
         }
 }
