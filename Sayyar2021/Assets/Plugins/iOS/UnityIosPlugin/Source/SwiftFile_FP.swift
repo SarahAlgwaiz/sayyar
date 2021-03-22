b7898c44ea5bd705cacc008fd4d3463b0cf4bf0e
Shahad Alkaltham <48352913+shhdSU@users.noreply.github.com>
2021-03-22 23:32:20 +0300

    Token successfully taken !!!

--- Sayyar2021/Assets/Plugins/iOS/UnityIosPlugin/Source/SwiftFile_FP.swift
+++ Sayyar2021/Assets/Plugins/iOS/UnityIosPlugin/Source/SwiftFile_FP.swift
@@ -6,12 +6,27 @@
 //
 
 import Foundation
+import DeviceCheck
 
 @objc public class SwiftFile_FP : NSObject {
     
     @objc public static let shared = SwiftFile_FP()
     @objc public func fingerprintMethodInSwift(){
+
+        if DCDevice.current.isSupported { // Always test for availability.
+            DCDevice.current.generateToken { token, error in
+                guard error == nil else { return}
+                print("ttttttooooooookkkkkkkeeeeeennnnn!!!!!!!!!!!!@@@@@@@@@@@@@")
+                var dt = token?.map{String(format: "%02.2hhx", $0)}.joined()
+                print(dt)
+
+                // Send the token to your server.
+            }
+        }
         
         print("It wooorrrrrrkkkk fiiinnnaaaallllyyyy !!!!!!!!!!!!@@@@@@@@@@@@@")
     }
+    
+
+    
 }
